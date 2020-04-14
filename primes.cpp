8a8dc2797ab0d24790063648f0fb90ab8c1fa686
Avery <agrieve@umich.edu>
2020-04-14 02:20:51 -0400

    it has a timer now

--- primes.cpp
+++ primes.cpp
@@ -1,6 +1,7 @@
 #include <math.h>
 #include <vector>
 #include <iostream>
+#include <chrono>
 using namespace std;
 
 vector<int> checkNumber(int checkNum, vector<int> primeList) {
@@ -19,15 +20,22 @@ vector<int> checkNumber(int checkNum, vector<int> primeList) {
 
 int main() {
 	
+	auto start = chrono::steady_clock::now();
+
 	cout << "1 is prime\n" << "2 is prime\n" << "3 is prime\n" << "5 is prime\n";
 	int z=6;
 	vector<int> primeList = {2, 3, 5};
 	int b = primeList.size();
 
-	while (b < 50){
+	while (b < 10000){
 		primeList = checkNumber(z, primeList);
 		z++;
 		b = primeList.size();
 	}
+	auto end = chrono::steady_clock::now();
+
+	cout << "Elapsed time to calculate " << b << " prime numbers: " 
+		 << chrono::duration_cast<chrono::milliseconds>(end-start).count()
+		 << " ms" << endl;
 	return 0;
 }
